43ba60bd2895867e214864d181de005dd43aada3
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-04-23 13:17:15 +0200

    add composite pattern

---
+++ structural/composite/Composite.md
@@ -0,0 +1,21 @@
+# Composite
+
+### When to use the pattern?
+Composite pattern is useful when a group of different objects should be treated uniformly.
+Say, when you need to do the same operation for each object in a group, and you don't want to distinguish their types,
+you just need to call the same method for each object.
+
+### Example
+The easiest and most explicit example would be a company with an employee hierarchy.
+There can be software developers which are subordinates of a project manager. At the same time,
+the manager is headed by a unit manager which is a subordinate of a company CEO.
+
+The idea of the pattern is to create a tree-like structure where the root is a composite object (CEO in our example) 
+which includes the child nodes (managers and developers in our example).
+Imagine the situation when we need to list the details of each company employee. Instead of iterating through all employees 
+and requesting the details for an individual person, we are calling CEO's `get_details()` method which prints all subordinates' details.  
+
+**The pattern has several key principles:**
+* Component interface. Defines an interface which is implemented by all node of the tree.
+* Leaf node. A primitive object (developer in the example).
+* Composite node. Stores the list of child components and implements the component interface for each child (Manager in the example).
---
+++ structural/composite/composite.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from typing import Set
+
+
+class EmployeeInterface(ABC):
+    """
+    The class defines an interface which has to be
+    implemented by all employees of a tree.
+    """
+
+    @abstractmethod
+    def print_details(self):
+        """
+        Prints the employee details
+        """
+
+
+class Employee(EmployeeInterface):
+
+    def __init__(self, name: str, job: str, salary: int):
+        self.name = name
+        self.job = job
+        self.salary = salary
+
+    def print_details(self):
+        print(f"Name: {self.name}, Job: {self.job}, Salary: {self.salary}")
+
+
+class Manager(EmployeeInterface):
+
+    def __init__(self, name: str, job: str, salary: int):
+        self.name = name
+        self.job = job
+        self.salary = salary
+        self._subordinates: Set[EmployeeInterface] = set()
+
+    def print_details(self):
+        for subordinate in self._subordinates:
+            subordinate.print_details()
+        print(f"Name: {self.name}, Job: {self.job}, Salary: {self.salary}")
+
+    def add_subordinate(self, subordinate: EmployeeInterface):
+        self._subordinates.add(subordinate)
+
+
+if __name__ == "__main__":
+    java_dev = Employee("Alex", "Java Developer", 50000)
+    python_dev = Employee("James", "Python Developer", 75000)
+
+    project_manager = Manager("Tom", "Project Manager", 100000)
+    project_manager.add_subordinate(java_dev)
+    project_manager.add_subordinate(python_dev)
+
+    unit_manager = Manager("Ann", "Unit Manager", 150000)
+    unit_manager.add_subordinate(project_manager)
+
+    ceo = Manager("Jeff", "CEO", 500000)
+    ceo.add_subordinate(unit_manager)
+
+    ceo.print_details()
