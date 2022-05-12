49a20d2810fb3d9171ea2aa0438a7e096992c668
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-05-12 11:23:05 +0200

    add visitor pattern

---
+++ behavioral/visitor/Visitor.md
@@ -0,0 +1,23 @@
+# Visitor
+
+### When to use the pattern?
+When there is a need to extend existing hierarchy with new capabilities but the code of this hierarchy can't be changed.
+Also, when the responsibility of the new capabilities differs from the hierarchy responsibility.
+
+If you create a new class for the new functionality and include an object of the hierarchy to this class, you might end up
+having a huge if/else block for handling different types of objects. In this case, the double dispatch technique might be a solution.
+
+### Example
+You have a set of classes that represent DB records (`DBUserModel`, `DBPostModel`) and need to add serialization capabilities.
+You can't change the code of `DBUserModel`, `DBPostModel` because it is pretty stable and also do not want to extend it with unnecessary responsibilities.
+
+What you can do is create a `SerializationVisitor` class that will have a set of methods for serializing each of the records
+by accepting it: `serialize_user` or `serialize_post`. Then, we will use the double dispatch technique: `DBUserModel`, `DBPostModel` 
+will implement an `AcceptVisitorInterface` with a public method `accept_visitor` that will accept the visitor instance and call the corresponding
+serialization method.
+
+
+
+**Advantage of the pattern is that we do not mix responsibilities, follow open/closed principle, and we are able to change the visitor on the fly.**
+
+**The drawback is that it introduces circular dependencies: visitor depends on the model interface, model depends on the visitor interface.**
---
+++ behavioral/visitor/visitor.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+import uuid
+from abc import ABC, abstractmethod
+from datetime import datetime
+import json
+
+
+class SerializationVisitorInterface(ABC):
+    """
+    The interface declares methods for serializing
+    different types of DB models classes.
+    """
+
+    @abstractmethod
+    def serialize_user(self, user: DBUserModel) -> str:
+        """
+        Serializes DBUserModel object.
+        """
+
+    @abstractmethod
+    def serialize_post(self, post: DBPostModel) -> str:
+        """
+        Serializes DBPostModel object.
+        """
+
+
+class SerializationVisitor(SerializationVisitorInterface):
+    """
+    A concrete serializer.
+    """
+
+    def serialize_user(self, user: DBUserModel) -> str:
+        serialized: dict = {
+            "id": user.id,
+            "created_at": user.created_at,
+            "username": user.username,
+        }
+        return json.dumps(serialized)
+
+    def serialize_post(self, post: DBPostModel) -> str:
+        serialized: dict = {
+            "id": post.id,
+            "created_at": post.created_at,
+            "description": post.description,
+        }
+        return json.dumps(serialized)
+
+
+class AcceptVisitorInterface(ABC):
+    """
+    The interface must be implemented by all
+    objects that support a serializer visitor.
+    """
+
+    @abstractmethod
+    def accept_visitor(self, visitor: SerializationVisitorInterface) -> str:
+        """
+        Must call the corresponding method in the given visitor (double dispatch).
+        """
+
+
+class BaseDBModel(AcceptVisitorInterface, ABC):
+    """
+    Implements a common behavior for all concrete DB models.
+    """
+
+    def __init__(self, record_params: dict):
+        self.id: str = record_params["id"]
+        self.created_at: str = record_params["created_at"]
+
+
+class DBUserModel(BaseDBModel):
+    """
+    Represents a record in "Users" table.
+    """
+
+    def __init__(self, record_params: dict):
+        super(DBUserModel, self).__init__(record_params)
+        self.username: str = record_params["username"]
+
+    def accept_visitor(self, visitor: SerializationVisitorInterface) -> str:
+        return visitor.serialize_user(self)
+
+
+class DBPostModel(BaseDBModel):
+    """
+    Represents a record in "Posts" table.
+    """
+
+    def __init__(self, record_params: dict):
+        super(DBPostModel, self).__init__(record_params)
+        self.description: str = record_params["description"]
+
+    def accept_visitor(self, visitor: SerializationVisitorInterface) -> str:
+        return visitor.serialize_post(self)
+
+
+if __name__ == "__main__":
+    user_obj = DBUserModel(
+        {"id": str(uuid.uuid4()), "created_at": datetime.now().isoformat(), "username": "John"}
+    )
+    post_obj = DBPostModel(
+        {"id": str(uuid.uuid4()), "created_at": datetime.now().isoformat(), "description": "Sports Event"}
+    )
+
+    visitor_obj = SerializationVisitor()
+    for model in [user_obj, post_obj]:
+        print(model.accept_visitor(visitor_obj))
